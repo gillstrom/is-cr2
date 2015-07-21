b6207e90d05b78fda2e270647ce976a8b33937a4
Andreas Gillström <andreasgillstrom@gmail.com>
2015-07-21 07:14:31 +0200

    Tweak

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = function (buf) {
-	if (!buf || buf.length < 6) {
+	if (!buf || buf.length < 10) {
 		return false;
 	}
 
-	return buf[0] === 0x49 && buf[1] === 0x49 && buf[2] === 0x2a && buf[3] === 0x0 && buf[4] === 0x10 && buf[5] === 0x0;
+	return buf[0] === 0x49 && buf[1] === 0x49 && buf[2] === 0x2a && buf[3] === 0x0 && buf[4] === 0x10 && buf[5] === 0x0 && buf[6] === 0x0 && buf[7] === 0x0 && buf[8] === 0x43 && buf[9] === 0x52;
 };
