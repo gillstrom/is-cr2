9ba499fcf24def80259c85e1f4b361549ea62ebb
Andreas Gillström <andreasgillstrom@gmail.com>
2015-07-21 21:24:34 +0200

    Tweaks

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ module.exports = function (buf) {
 		return false;
 	}
 
-	return buf[8] === 0x43 && buf[9] === 0x52;
+	return (buf[0] === 0x49 && buf[1] === 0x49 && buf[2] === 0x2A && buf[3] === 0x0) || (buf[0] === 0x4D && buf[1] === 0x4D && buf[2] === 0x0 && buf[3] === 0x2A) && buf[8] === 0x43 && buf[9] === 0x52;
 };
