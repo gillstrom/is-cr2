e38c0a9e18efd0be819c7e5d330e902bba442ae8
Andreas Gillström <andreasgillstrom@gmail.com>
2015-07-21 07:22:43 +0200

    Tweak

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ module.exports = function (buf) {
 		return false;
 	}
 
-	return buf[0] === 0x49 && buf[1] === 0x49 && buf[2] === 0x2a && buf[3] === 0x0 && buf[4] === 0x10 && buf[5] === 0x0 && buf[6] === 0x0 && buf[7] === 0x0 && buf[8] === 0x43 && buf[9] === 0x52;
+	return (buf[8] === 0x43 && buf[9] === 0x52) && (buf[10] === 0x02 || buf[10] === 0x00);
 };
